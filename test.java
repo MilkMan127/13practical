1365843c15dc41c70a6ec558e06eaa2cc70ea726
MilkMan127 <4394348@myuwc.ac.za>
2026-02-23 16:00:11 +0200

    Add imports and Node class to test.java

--- test.java
+++ test.java
@@ -1,1 +1,15 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Random;
 
+public class timeMethods {
+
+    static class Node {
+        int key;
+        String data;
+
+        Node(int k, String d) {
+            key = k;
+            data = d;
+        }
+    }
